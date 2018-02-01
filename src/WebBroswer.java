bfcc44f1f6d3e45f756bfe502587fdc7729a0ca8
cys_star <cys_star@naver.com>
2018-02-01 17:01:56 +0900

    Change Structure

--- src/WebBroswer.java
+++ src/WebBroswer.java
@@ -1,15 +1,21 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.net.ssl.HttpsURLConnection;
+
 
 public class WebBroswer implements Runnable {
 	private Socket webSocket;
 	private final String webURL = "www.naver.com";// http://www.seoul.go.kr/main/index.html
-	private final int webPort = 443;
+	private final String webPath = "/index.html";
+	private final int webPort = 443;//https´Â 443
 	private BufferedWriter writer;
 	private BufferedReader reader;
 
@@ -46,10 +52,20 @@ public class WebBroswer implements Runnable {
 
 	public void receiveMessage() {
 		String message = null;
+		FileWriter fileWriter = null;
+		long time = System.currentTimeMillis(); 
+
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyMMddhhmm");
+
+		String str = dayTime.format(new Date(time));
+
+
+		
 		Logger.println("Connected!");
 
 		try {
-			writer.write("GET /index.html HTTP/1.1\r\n");
+			fileWriter = new FileWriter(new File(webURL+str + ".txt"));
+			writer.write("GET " + webPath + " HTTP/1.1\r\n");
 			writer.write("Content-Type: text/html\r\n");
 			writer.write("Host: " + webURL + "\r\n\r\n");
 			writer.flush();
@@ -57,7 +73,12 @@ public class WebBroswer implements Runnable {
 			Logger.println(webSocket.getLocalAddress() + " " + webSocket.getLocalSocketAddress());
 			while ((message = reader.readLine()) != null) {
 				System.out.println(message);
+				fileWriter.write(message + "\r\n");
+				if(message.equals("0"))
+					break;
 			}
+			fileWriter.flush();
+			fileWriter.close();
 		} catch (Exception e) {
 			e.getStackTrace();
 		}
