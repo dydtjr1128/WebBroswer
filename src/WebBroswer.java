24c8a41c12b1983488a0506258795f254a95442b
cys_star <cys_star@naver.com>
2018-02-01 16:43:12 +0900

    Change Structure

--- src/WebBroswer.java
+++ src/WebBroswer.java
@@ -21,6 +21,7 @@ public class WebBroswer implements Runnable {
 	@Override
 	public void run() {
 		httpsConnection();
+		receiveMessage();
 	}
 
 	public void httpConnection() {
@@ -28,21 +29,6 @@ public class WebBroswer implements Runnable {
 			webSocket = new Socket(webURL, webPort);
 			writer = new BufferedWriter(new OutputStreamWriter(webSocket.getOutputStream(), "UTF8"));
 			reader = new BufferedReader(new InputStreamReader(webSocket.getInputStream(), "UTF8"));
-			Logger.println("Connect!");
-
-			String message = null;
-
-			writer.write("GET /index.html HTTP/1.1\r\n");
-			writer.write("Content-Type: text/html\r\n");
-			writer.write("Host: " + webURL + "\r\n\r\n");
-			writer.flush();
-
-			System.out.println(webSocket.getLocalAddress() + " " + webSocket.getLocalSocketAddress());
-
-			while ((message = reader.readLine()) != null) {
-				System.out.println(message);
-			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -53,20 +39,27 @@ public class WebBroswer implements Runnable {
 			webSocket = HttpsURLConnection.getDefaultSSLSocketFactory().createSocket(webURL, webPort);
 			writer = new BufferedWriter(new OutputStreamWriter(webSocket.getOutputStream(), "UTF8"));
 			reader = new BufferedReader(new InputStreamReader(webSocket.getInputStream(), "UTF8"));
-			Logger.println("Connect!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-			String message = null;
+	public void receiveMessage() {
+		String message = null;
+		Logger.println("Connected!");
 
+		try {
 			writer.write("GET /index.html HTTP/1.1\r\n");
 			writer.write("Content-Type: text/html\r\n");
 			writer.write("Host: " + webURL + "\r\n\r\n");
 			writer.flush();
-			
+
+			Logger.println(webSocket.getLocalAddress() + " " + webSocket.getLocalSocketAddress());
 			while ((message = reader.readLine()) != null) {
 				System.out.println(message);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			e.getStackTrace();
 		}
 	}
 
